bf6d88bc38184808fdae6fda1f47f2948e4e7b41
mepwork <mitchel.eppich.work@gmail.com>
2018-12-28 13:43:43 -0800

    merging

--- components/Video/index.js
+++ components/Video/index.js
@@ -1,21 +1,35 @@
-import React from "react"
+import React from "react";
 
 // let screen = window.innerWidth;
 // console.log("test", screen )
 
-const Video = () => {    
-    return (        
-        <div id="video" className="w-full h-full bg-yellow relative">  
-            <div>
-                <video className="w-full overflow-hidden" autoPlay loop>    
-                {/* {screen.innerWidth > 650 ? (          */}
-                    <source src="../../static/video/stt_1920.mp4" type="video/mp4"></source> 
-                    {/* ):            
-                    (<source src="../../static/video/stt_360.mp4" type="video/mp4"></source>) } */}                   
-                </video>
-            </div>
-        </div>        
-    )
-}
+const Video = () => {
+  return (
+    <div id="video" style={{}} className="bg-yellow">
+      <div
+        style={{ marginTop: "80px", marginLeft: "12%", marginBottom: "-3px" }}
+      >
+        <video
+          className="overflow-hidden"
+          loop
+          muted
+          id="videoTag"
+          autoPlay
+          onClick={e => {
+            e.target.muted = !e.target.muted;
+          }}
+          style={{ width: "85%" }}
+        >
+          {}
+          {/* {screen.innerWidth > 650 ? (          */}
+          <source src="../../static/video/stt_1920.mp4" type="video/mp4" />
+          {/* ):            
+                    (<source src="../../static/video/stt_360.mp4" type="video/mp4"></source>) } */}
+        </video>
+        <div style={{ position: "absolute" }}>MUTED</div>
+      </div>
+    </div>
+  );
+};
 
-export default Video 
+export default Video;
